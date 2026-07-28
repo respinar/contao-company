6ff861ceee8f76f9cb4b16d73de37b5605087714
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 22:52:54 +0330

    Simplify config.php to just register company group

--- contao/config/config.php
+++ contao/config/config.php
@@ -10,23 +10,5 @@ declare(strict_types=1);
  * @license MIT
  */
 
-
 // Back end modules
 $GLOBALS['BE_MOD']['company'] = [];
-
-// Insert 'company' group after 'content'
-if (isset($GLOBALS['BE_MOD']['content'])) {
-    $companyModules = $GLOBALS['BE_MOD']['company'];
-    unset($GLOBALS['BE_MOD']['company']);
-
-    $keys = array_keys($GLOBALS['BE_MOD']);
-    $contentIndex = array_search('content', $keys, true);
-
-    if ($contentIndex !== false) {
-        $before = array_slice($GLOBALS['BE_MOD'], 0, $contentIndex + 1, true);
-        $after = array_slice($GLOBALS['BE_MOD'], $contentIndex + 1, null, true);
-        $GLOBALS['BE_MOD'] = array_merge($before, ['company' => $companyModules], $after);
-    } else {
-        $GLOBALS['BE_MOD']['company'] = $companyModules;
-    }
-}
