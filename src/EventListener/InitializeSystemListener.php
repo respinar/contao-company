161128b608a338dc21027f41bfefcae589d10677
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 22:01:33 +0330

    Add InitializeSystemListener to load backend icon stylesheet

---
+++ src/EventListener/InitializeSystemListener.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Company.
+ *
+ * (c) Hamid Peywasti 2026 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\CompanyBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+#[AsHook('initializeSystem')]
+class InitializeSystemListener
+{
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly ScopeMatcher $scopeMatcher,
+        private readonly Packages $packages,
+    ) {
+    }
+
+    /**
+     * Load the CSS file for the back end navigation group icon.
+     */
+    public function __invoke(): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (!$request || !$this->scopeMatcher->isBackendRequest($request)) {
+            return;
+        }
+
+        $GLOBALS['TL_CSS'][] = $this->packages->getUrl('css/backend.css', 'respinar_company');
+    }
+}
