8c8e15d52ffcc136b4b755046b7a3aa2dca584ee
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:05:13 +0330

    Apply ECS fixes to tl_company_location DCA

--- contao/dca/tl_company_location.php
+++ contao/dca/tl_company_location.php
@@ -15,62 +15,62 @@ use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_company_location'] = [
     'config' => [
-        'dataContainer'    => DC_Table::class,
+        'dataContainer' => DC_Table::class,
         'enableVersioning' => true,
-        'markAsCopy'       => 'title',
-        'sql'              => [
+        'markAsCopy' => 'title',
+        'sql' => [
             'keys' => [
-                'id'  => 'primary',
+                'id' => 'primary',
                 'pid' => 'index',
             ],
         ],
     ],
     'list' => [
         'sorting' => [
-            'mode'                    => DataContainer::MODE_TREE,
-            'rootPaste'               => true,
-            'panelLayout'             => 'filter;search',
+            'mode' => DataContainer::MODE_TREE,
+            'rootPaste' => true,
+            'panelLayout' => 'filter;search',
         ],
         'label' => [
-            'fields'                  => ['title'],
-            'format'                  => '%s',
+            'fields' => ['title'],
+            'format' => '%s',
         ],
     ],
 
     'palettes' => [
-        'default'                     => '{title_legend},title,alias;{desc_legend},description',
+        'default' => '{title_legend},title,alias;{desc_legend},description',
     ],
 
     'fields' => [
         'id' => [
-            'sql'                     => "int(10) unsigned NOT NULL auto_increment",
+            'sql' => 'int(10) unsigned NOT NULL auto_increment',
         ],
         'pid' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'sorting' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'tstamp' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'title' => [
-            'inputType'               => 'text',
-            'search'                  => true,
-            'eval'                    => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
-            'sql'                     => "varchar(255) NOT NULL default ''",
+            'inputType' => 'text',
+            'search' => true,
+            'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
         ],
         'alias' => [
-            'inputType'               => 'text',
-            'search'                  => true,
-            'eval'                    => ['rgxp' => 'alias', 'doNotCopy' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
-            'sql'                     => "varchar(128) BINARY NOT NULL default ''",
+            'inputType' => 'text',
+            'search' => true,
+            'eval' => ['rgxp' => 'alias', 'doNotCopy' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
+            'sql' => "varchar(128) BINARY NOT NULL default ''",
         ],
         'description' => [
-            'inputType'               => 'textarea',
-            'search'                  => true,
-            'eval'                    => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
-            'sql'                     => "text NULL",
+            'inputType' => 'textarea',
+            'search' => true,
+            'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
+            'sql' => 'text NULL',
         ],
     ],
 ];
