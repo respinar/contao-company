b6ee7831093538c5bfbfd043676a3fa390c123ce
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:05:01 +0330

    Apply ECS fixes to tl_company_category DCA

--- contao/dca/tl_company_category.php
+++ contao/dca/tl_company_category.php
@@ -15,48 +15,48 @@ use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_company_category'] = [
     'config' => [
-        'dataContainer'    => DC_Table::class,
+        'dataContainer' => DC_Table::class,
         'enableVersioning' => true,
-        'markAsCopy'       => 'title',
-        'sql'              => [
+        'markAsCopy' => 'title',
+        'sql' => [
             'keys' => [
-                'id'  => 'primary',
+                'id' => 'primary',
                 'pid' => 'index',
             ],
         ],
     ],
     'list' => [
         'sorting' => [
-            'mode'                    => DataContainer::MODE_TREE,
-            'rootPaste'               => true,
-            'panelLayout'             => 'filter;search',
+            'mode' => DataContainer::MODE_TREE,
+            'rootPaste' => true,
+            'panelLayout' => 'filter;search',
         ],
         'label' => [
-            'fields'                  => ['title'],
-            'format'                  => '%s',
+            'fields' => ['title'],
+            'format' => '%s',
         ],
         'global_operations' => [
             'new' => [
-                'href'                => 'act=create',
-                'class'               => 'header_new',
-                'attributes'          => 'onclick="Backend.getScrollOffset()"',
+                'href' => 'act=create',
+                'class' => 'header_new',
+                'attributes' => 'onclick="Backend.getScrollOffset()"',
             ],
             'toggleNodes' => [
-                'href'                => 'ptg=all',
-                'class'               => 'header_toggle',
+                'href' => 'ptg=all',
+                'class' => 'header_toggle',
             ],
             'all' => [
-                'href'                => 'act=select',
-                'class'               => 'header_edit_all',
-                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+                'href' => 'act=select',
+                'class' => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
             ],
         ],
         'operations' => [
             'edit',
             'copy',
             'copyChilds' => [
-                'href'                => 'act=copy&amp;childs=1',
-                'icon'                => 'copychilds.svg',
+                'href' => 'act=copy&amp;childs=1',
+                'icon' => 'copychilds.svg',
             ],
             'cut',
             'delete',
@@ -65,37 +65,37 @@ $GLOBALS['TL_DCA']['tl_company_category'] = [
     ],
     'fields' => [
         'id' => [
-            'sql'                     => "int(10) unsigned NOT NULL auto_increment",
+            'sql' => 'int(10) unsigned NOT NULL auto_increment',
         ],
         'pid' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'sorting' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'tstamp' => [
-            'sql'                     => "int(10) unsigned NOT NULL default 0",
+            'sql' => 'int(10) unsigned NOT NULL default 0',
         ],
         'title' => [
-            'inputType'               => 'text',
-            'search'                  => true,
-            'eval'                    => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
-            'sql'                     => "varchar(255) NOT NULL default ''",
+            'inputType' => 'text',
+            'search' => true,
+            'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
         ],
         'alias' => [
-            'inputType'               => 'text',
-            'search'                  => true,
-            'eval'                    => ['rgxp' => 'alias', 'doNotCopy' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
-            'sql'                     => "varchar(128) BINARY NOT NULL default ''",
+            'inputType' => 'text',
+            'search' => true,
+            'eval' => ['rgxp' => 'alias', 'doNotCopy' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
+            'sql' => "varchar(128) BINARY NOT NULL default ''",
         ],
         'description' => [
-            'inputType'               => 'textarea',
-            'search'                  => true,
-            'eval'                    => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
-            'sql'                     => "text NULL",
+            'inputType' => 'textarea',
+            'search' => true,
+            'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
+            'sql' => 'text NULL',
         ],
     ],
     'palettes' => [
-        'default'                     => '{title_legend},title,alias;{desc_legend},description',
+        'default' => '{title_legend},title,alias;{desc_legend},description',
     ],
 ];
