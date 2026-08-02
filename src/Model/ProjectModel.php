bd5708a7db627f6cef655e7be5e524d94494007a
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:07:23 +0330

    Apply ECS fixes to ProjectModel

--- src/Model/ProjectModel.php
+++ src/Model/ProjectModel.php
@@ -12,33 +12,33 @@ declare(strict_types=1);
 
 namespace Respinar\CompanyBundle\Model;
 
+use Contao\Date;
 use Contao\Model;
 use Contao\Model\Collection;
-use Contao\Date;
 
 /**
- * Reads and writes projects
+ * Reads and writes projects.
  *
- * @property integer  $id
- * @property integer  $pid
- * @property integer  $tstamp
- * @property string   $title
- * @property string   $alias
- * @property integer  $author
- * @property integer  $date
- * @property integer  $completionDate
- * @property boolean  $featured
- * @property string   $summary
- * @property string   $description
- * @property string   $singleSRC
- * @property string   $multiSRC
- * @property boolean  $published
- * @property string   $start
- * @property string   $stop
+ * @property int    $id
+ * @property int    $pid
+ * @property int    $tstamp
+ * @property string $title
+ * @property string $alias
+ * @property int    $author
+ * @property int    $date
+ * @property int    $completionDate
+ * @property bool   $featured
+ * @property string $summary
+ * @property string $description
+ * @property string $singleSRC
+ * @property string $multiSRC
+ * @property bool   $published
+ * @property string $start
+ * @property string $stop
  *
- * @method static ProjectModel|null findById($id, array $opt=array())
- * @method static ProjectModel|null findByPk($id, array $opt=array())
- * @method static ProjectModel|null findOneBy($col, $val, array $opt=array())
+ * @method static ProjectModel|null             findById($id, array $opt=array())
+ * @method static ProjectModel|null             findByPk($id, array $opt=array())
+ * @method static ProjectModel|null             findOneBy($col, $val, array $opt=array())
  * @method static Collection<ProjectModel>|null findBy($col, $val, array $opt=array())
  * @method static Collection<ProjectModel>|null findAll(array $opt=array())
  */
@@ -46,47 +46,41 @@ class ProjectModel extends Model
 {
     protected static $strTable = 'tl_company_project';
 
-  /**
-	 * Find published projects items by their parent ID
-	 *
-	 * @param array<int> $arrPids
-	 * @param array<string, mixed> $arrOptions
-	 *
-	 * @return Collection<ProjectModel>|null
-	 */
-	 public static function findPublishedByPids(array $arrPids, ?bool $blnFeatured=null, int $intLimit=0, int $intOffset=0, array $arrOptions=[]): ?Collection
-	{
-		if (empty($arrPids) || !\is_array($arrPids))
-		{
-			return null;
-		}
+    /**
+     * Find published projects items by their parent ID.
+     *
+     * @param array<int>           $arrPids
+     * @param array<string, mixed> $arrOptions
+     *
+     * @return Collection<ProjectModel>|null
+     */
+    public static function findPublishedByPids(array $arrPids, bool|null $blnFeatured = null, int $intLimit = 0, int $intOffset = 0, array $arrOptions = []): Collection|null
+    {
+        if (empty($arrPids) || !\is_array($arrPids)) {
+            return null;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid IN(" . implode(',', array_map('\intval', $arrPids)) . ")");
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid IN(".implode(',', array_map('\intval', $arrPids)).')'];
 
-		if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=0";
-		}
+        if (true === $blnFeatured) {
+            $arrColumns[] = "$t.featured=1";
+        } elseif (false === $blnFeatured) {
+            $arrColumns[] = "$t.featured=0";
+        }
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order']  = "$t.date DESC";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.date DESC";
+        }
 
-		$arrOptions['limit']  = $intLimit;
-		$arrOptions['offset'] = $intOffset;
+        $arrOptions['limit'] = $intLimit;
+        $arrOptions['offset'] = $intOffset;
 
-		return static::findBy($arrColumns, null, $arrOptions);
-	}
+        return static::findBy($arrColumns, null, $arrOptions);
+    }
 }
