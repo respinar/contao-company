6e78db1d958fe7f257f66f65de8216c61684f220
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:06:56 +0330

    Apply ECS fixes to CompanyClientLabelCallbackListener

--- src/EventListener/DataContainer/CompanyClientLabelCallbackListener.php
+++ src/EventListener/DataContainer/CompanyClientLabelCallbackListener.php
@@ -24,15 +24,15 @@ class CompanyClientLabelCallbackListener
      */
     public function __invoke(array $row): string
     {
-        $label = '<strong>' . $row['name'] . '</strong>';
+        $label = '<strong>'.$row['name'].'</strong>';
 
         if ($row['logo']) {
             $uuid = StringUtil::binToUuid($row['logo']);
-            $label = Image::getHtml($uuid, '', 'style="max-height:32px;max-width:50px;vertical-align:middle;margin-right:8px;"') . ' ' . $label;
+            $label = Image::getHtml($uuid, '', 'style="max-height:32px;max-width:50px;vertical-align:middle;margin-right:8px;"').' '.$label;
         }
 
         if ($row['industry']) {
-            $label .= ' <span style="color:#999;padding-left:3px">[' . $row['industry'] . ']</span>';
+            $label .= ' <span style="color:#999;padding-left:3px">['.$row['industry'].']</span>';
         }
 
         return $label;
