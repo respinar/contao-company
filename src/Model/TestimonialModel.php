e7a5655a3547b72d8434ff059fe396c53fc704f5
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:07:23 +0330

    Apply ECS fixes to TestimonialModel

--- src/Model/TestimonialModel.php
+++ src/Model/TestimonialModel.php
@@ -12,57 +12,51 @@ declare(strict_types=1);
 
 namespace Respinar\CompanyBundle\Model;
 
+use Contao\Date;
 use Contao\Model;
 use Contao\Model\Collection;
-use Contao\Date;
 
 class TestimonialModel extends Model
 {
     protected static $strTable = 'tl_company_testimonial';
 
     /**
-	 * Find published projects items by their parent ID
-	 *
-	 * @param array<int> $arrPids
-	 * @param array<string, mixed> $arrOptions
-	 *
-	 * @return Collection<TestimonialModel>|null
-	 */
-	 public static function findPublishedByPids(array $arrPids, ?bool $blnFeatured=null, int $intLimit=0, int $intOffset=0, array $arrOptions=[]): ?Collection
-	{
-		if (empty($arrPids) || !\is_array($arrPids))
-		{
-			return null;
-		}
+     * Find published projects items by their parent ID.
+     *
+     * @param array<int>           $arrPids
+     * @param array<string, mixed> $arrOptions
+     *
+     * @return Collection<TestimonialModel>|null
+     */
+    public static function findPublishedByPids(array $arrPids, bool|null $blnFeatured = null, int $intLimit = 0, int $intOffset = 0, array $arrOptions = []): Collection|null
+    {
+        if (empty($arrPids) || !\is_array($arrPids)) {
+            return null;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid IN(" . implode(',', array_map('\intval', $arrPids)) . ")");
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid IN(".implode(',', array_map('\intval', $arrPids)).')'];
 
-		if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=0";
-		}
+        if (true === $blnFeatured) {
+            $arrColumns[] = "$t.featured=1";
+        } elseif (false === $blnFeatured) {
+            $arrColumns[] = "$t.featured=0";
+        }
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order']  = "$t.date DESC";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.date DESC";
+        }
 
-		$arrOptions['limit']  = $intLimit;
-		$arrOptions['offset'] = $intOffset;
+        $arrOptions['limit'] = $intLimit;
+        $arrOptions['offset'] = $intOffset;
 
-		return static::findBy($arrColumns, null, $arrOptions);
-	}
+        return static::findBy($arrColumns, null, $arrOptions);
+    }
 
     /**
      * Generates the label for a testimonial record.
@@ -71,6 +65,6 @@ class TestimonialModel extends Model
      */
     public static function listTestimonials(array $row): string
     {
-        return '<div class="tl_content_left">' . ($row['title'] ?? '') . '</div>';
+        return '<div class="tl_content_left">'.($row['title'] ?? '').'</div>';
     }
 }
