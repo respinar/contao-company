06776413200e9a27ced70d708729b5dce1988689
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:07:11 +0330

    Apply ECS fixes to ClientModel

--- src/Model/ClientModel.php
+++ src/Model/ClientModel.php
@@ -12,45 +12,41 @@ declare(strict_types=1);
 
 namespace Respinar\CompanyBundle\Model;
 
+use Contao\Date;
 use Contao\Model;
 use Contao\Model\Collection;
-use Contao\Date;
 
 class ClientModel extends Model
 {
     protected static $strTable = 'tl_company_client';
 
-
     /**
-	 * Find published news items by their parent ID
-	 *
-	 * @param integer $intId      The news archive ID
-	 * @param integer $intLimit   An optional limit
-	 * @param array   $arrOptions An optional options array
-	 *
-	 * @return Collection<ClientModel>|null A collection of models or null if there are no news
-	 */
-	public static function findPublishedByPid(int $intId, int $intLimit=0, array $arrOptions=[]): ?Collection
-	{
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid=?");
+     * Find published news items by their parent ID.
+     *
+     * @param int   $intId      The news archive ID
+     * @param int   $intLimit   An optional limit
+     * @param array $arrOptions An optional options array
+     *
+     * @return Collection<ClientModel>|null A collection of models or null if there are no news
+     */
+    public static function findPublishedByPid(int $intId, int $intLimit = 0, array $arrOptions = []): Collection|null
+    {
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid=?"];
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order'] = "$t.sorting ASC";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.sorting ASC";
+        }
 
-		if ($intLimit > 0)
-		{
-			$arrOptions['limit'] = $intLimit;
-		}
+        if ($intLimit > 0) {
+            $arrOptions['limit'] = $intLimit;
+        }
 
-		return static::findBy($arrColumns, array($intId), $arrOptions);
-	}
+        return static::findBy($arrColumns, [$intId], $arrOptions);
+    }
 }
