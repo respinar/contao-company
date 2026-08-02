6a957291b5a0e7dccba4d55ab9a3b3ea7b3ac4d2
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-03 00:05:44 +0330

    Apply ECS fixes to tl_user and tl_user_group DCA

--- contao/dca/tl_user.php
+++ contao/dca/tl_user.php
@@ -15,56 +15,54 @@ use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
 
 // Extend the default palettes
 PaletteManipulator::create()
-	->addLegend('clients_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
-	->addField('clients', 'clients_legend', PaletteManipulator::POSITION_APPEND)
-	->applyToPalette('extend', 'tl_user')
-	->applyToPalette('custom', 'tl_user')
+    ->addLegend('clients_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('clients', 'clients_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('extend', 'tl_user')
+    ->applyToPalette('custom', 'tl_user')
 ;
 
 // Add fields to tl_user
-$GLOBALS['TL_DCA']['tl_user']['fields']['clients'] = array
-(
-	'inputType'               => 'checkbox',
-	'foreignKey'              => 'tl_company_client_group.title',
-	'eval'                    => array('multiple'=>true),
-	'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
-	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
-);
-
+$GLOBALS['TL_DCA']['tl_user']['fields']['clients'] =
+[
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_company_client_group.title',
+    'eval' => ['multiple' => true],
+    'sql' => ['type' => 'blob', 'length' => AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull' => false],
+    'relation' => ['type' => 'hasMany', 'load' => 'lazy'],
+];
 
 // Extend the default palettes
 PaletteManipulator::create()
-	->addLegend('projects_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
-	->addField('projects', 'projects_legend', PaletteManipulator::POSITION_APPEND)
-	->applyToPalette('extend', 'tl_user')
-	->applyToPalette('custom', 'tl_user')
+    ->addLegend('projects_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('projects', 'projects_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('extend', 'tl_user')
+    ->applyToPalette('custom', 'tl_user')
 ;
 
 // Add fields to tl_user
-$GLOBALS['TL_DCA']['tl_user']['fields']['projects'] = array
-(
-	'inputType'               => 'checkbox',
-	'foreignKey'              => 'tl_company_project_archive.title',
-	'eval'                    => array('multiple'=>true),
-	'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
-	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
-);
-
+$GLOBALS['TL_DCA']['tl_user']['fields']['projects'] =
+[
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_company_project_archive.title',
+    'eval' => ['multiple' => true],
+    'sql' => ['type' => 'blob', 'length' => AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull' => false],
+    'relation' => ['type' => 'hasMany', 'load' => 'lazy'],
+];
 
 // Extend the default palettes
 PaletteManipulator::create()
-	->addLegend('testimonials_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
-	->addField('testimonials', 'testimonials_legend', PaletteManipulator::POSITION_APPEND)
-	->applyToPalette('extend', 'tl_user')
-	->applyToPalette('custom', 'tl_user')
+    ->addLegend('testimonials_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('testimonials', 'testimonials_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('extend', 'tl_user')
+    ->applyToPalette('custom', 'tl_user')
 ;
 
 // Add fields to tl_user
-$GLOBALS['TL_DCA']['tl_user']['fields']['testimonials'] = array
-(
-	'inputType'               => 'checkbox',
-	'foreignKey'              => 'tl_company_testimonials_archive.title',
-	'eval'                    => array('multiple'=>true),
-	'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
-	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
-);
+$GLOBALS['TL_DCA']['tl_user']['fields']['testimonials'] =
+[
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_company_testimonials_archive.title',
+    'eval' => ['multiple' => true],
+    'sql' => ['type' => 'blob', 'length' => AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull' => false],
+    'relation' => ['type' => 'hasMany', 'load' => 'lazy'],
+];
