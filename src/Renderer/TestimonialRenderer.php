8ef6023d0a302e65b5a4bfa469812b1725fc8e1b
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-04 11:06:52 +0330

    Complete testimonial template and renderer updates, update remaining DCA and XLF files

--- src/Renderer/TestimonialRenderer.php
+++ src/Renderer/TestimonialRenderer.php
@@ -14,12 +14,23 @@ namespace Respinar\CompanyBundle\Renderer;
 
 use Contao\Config;
 use Contao\ContentModel;
+use Contao\CoreBundle\Image\Studio\Studio;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
 use Contao\Date;
 use Contao\FrontendTemplate;
+use Respinar\CompanyBundle\Model\ClientModel;
+use Respinar\CompanyBundle\Model\ProjectModel;
 use Respinar\CompanyBundle\Model\TestimonialModel;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class TestimonialRenderer
 {
+    public function __construct(
+        private readonly Studio $studio,
+        private readonly ContentUrlGenerator $contentUrlGenerator,
+    ) {
+    }
+
     public function renderTestimonial(TestimonialModel $testimonial, ContentModel $model): string
     {
         $template = new FrontendTemplate(
@@ -39,6 +50,24 @@ final class TestimonialRenderer
         $template->dateFormatted = !empty($testimonial->date)
             ? Date::parse(Config::get('dateFormat'), (int) $testimonial->date)
             : '';
+
+        if (!empty($testimonial->project)) {
+            $project = ProjectModel::findById($testimonial->project);
+            if (null !== $project) {
+                $template->projectTitle = $project->title;
+
+                $template->projectUrl = $this->contentUrlGenerator->generate($project, [], UrlGeneratorInterface::ABSOLUTE_PATH);
+            }
+        }
+
+        if (!empty($testimonial->client)) {
+            $client = ClientModel::findById($testimonial->client);
+            if (null !== $client) {
+                $template->clientName = $client->name;
+
+                $template->clientUrl = $this->contentUrlGenerator->generate($client, [], UrlGeneratorInterface::ABSOLUTE_PATH);
+            }
+        }
 
         return $template->parse();
     }
