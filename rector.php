0f1d13993d738b5da53176b40521c14f3280724d
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-15 16:27:58 +0330

    Update tooling configuration
    - Add .php-cs-fixer.dist.php for PHP-CS-Fixer config
    
    - Add .php-cs-fixer.cache to .gitignore
    
    - Replace symplify/easy-coding-standard with contao/rector in composer.json
    
    - Remove Symfony 6.4 set from rector.php to avoid conflicts

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -19,7 +18,6 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::PHP_83,
         SetList::TYPE_DECLARATION,
         SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SymfonySetList::SYMFONY_64,
+        SetList::DEAD_CODE
     ]);
 };
